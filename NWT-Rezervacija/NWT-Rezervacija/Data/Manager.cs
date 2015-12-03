1c936cc1d4ed390b1f3f08e2f20885fc98b97933
hhodzi00 <hhodzi00@fesb.hr>
2015-12-03 16:50:41 +0100

    Dodane klase

---
+++ NWT-Rezervacija/NWT-Rezervacija/Data/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NWT_Rezervacija.Data
+{
+    public class Manager
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ClubLeader { get; set; }
+
+
+
+        public ICollection<Reservation> Reservations { get; set; }
+    }
+}
---
+++ NWT-Rezervacija/NWT-Rezervacija/Data/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NWT_Rezervacija.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Created { get; set; }
+
+
+        public ICollection<Reservation> Reservations { get; set; }
+
+
+    }
+}
